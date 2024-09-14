e9a0cbf06119f91dabc9aca1ff87807c5c16d918
Tina <tinajouzdani@gmail.com>
2024-09-14 11:33:20 +0330

    m

--- Models/laptop.cs
+++ Models/laptop.cs
@@ -1,11 +1,11 @@
 ﻿namespace DotNetHW2;
 
-public class pen:Item
+public class Pen:Item
 {
     private string brand;
     private string color;
 
-    public pen(int price, int numbersOfBooks, string address, string brand, string color) : base(price, numbersOfBooks, address)
+    public Pen(int price, int numbersOfBooks, string address, string brand, string color) : base(price, numbersOfBooks, address)
     {
         this.brand = brand;
         this.color = color;
