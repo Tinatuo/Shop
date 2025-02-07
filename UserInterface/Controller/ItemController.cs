a4da19d8702096eac8413ef841241f09fb759899
Tina <tinajouzdani@gmail.com>
2025-02-08 00:43:57 +0330

    working on controllers

---
+++ UserInterface/Controller/ItemController.cs
@@ -0,0 +1,65 @@
+﻿namespace UserInterface.Controller;
+using Microsoft.AspNetCore.Mvc;
+using Service;
+using DotNetHW2;
+
+[ApiController]
+[Route("item-api")]
+public class ItemController:ControllerBase
+{
+    [HttpPost("add-Book")]
+    public ContentResult AddBook(int price, int numbersOfItem, string address, int numberOfPages, string publication)
+    {
+        bool result=Service.ItemService.GetItemService().AddBook(price, numbersOfItem, address, numberOfPages, publication);
+        if (result)
+        {
+            return Content("book added");
+        }
+        else
+        {
+            return Content("book not added");
+        }
+    }
+
+    [HttpPost("add-Pen")]
+    public ContentResult AddPen(int price, int numbersOfItem, string address, string brand, string color)
+    {
+        bool result=Service.ItemService.GetItemService().AddPen(price, numbersOfItem, address, brand, color);
+        if (result)
+        {
+            return Content("pen added");
+        }
+        else
+        {
+            return Content("pen not added");
+        }
+    }
+
+    [HttpDelete("delete-item")]
+    public ContentResult removeItem(Item item)
+    {
+        bool result=Service.ItemService.GetItemService().RemoveItem(item);
+        if (result)
+        {
+            return Content("item removed");
+        }
+        else
+        {
+            return Content("item not removed");
+        }
+    }
+
+    [HttpPatch("Buy-Item")]
+    public ContentResult BuyItem(Item item)
+    {
+        bool result=Service.ItemService.GetItemService().BuyItem(item);
+        if (result)
+        {
+            return Content("item buyed");
+        }
+        else
+        {
+            return Content("item not buyed");
+        }
+    }
+}
---
+++ UserInterface/Controller/UserController.cs
@@ -0,0 +1,82 @@
+﻿using DotNetHW2;
+
+namespace UserInterface.Controller;
+using Microsoft.AspNetCore.Mvc;
+
+
+[ApiController]
+[Route("item-api")]
+public class UserController:ControllerBase
+{
+    [HttpPost("login-user")]
+    public ContentResult Login(string username, string password)
+    {
+        bool result = Service.UserService.GetUserService().Login(username, password);
+        if (result)
+        {
+            return Content("Login successful");
+        }
+        else
+        {
+            return Content("Login failed");
+        }
+        
+    }
+
+    [HttpPost("sign-up-user")]
+    public ContentResult Signup(string name, string pass, string address, UserType userType)
+    {
+        bool result=Service.UserService.GetUserService().SignUp(name, pass, address, userType);
+        if (result)
+        {
+            return Content("Signup successful");
+        }
+        else
+        {
+            return Content("Signup failed");
+        }
+    }
+
+    [HttpPut("change-address-user")]
+    public ContentResult ChangeAddress(string newAddress)
+    {
+        bool result = Service.UserService.GetUserService().ChangeAddress(newAddress);
+        if (result)
+        {
+            return Content("Address changed");
+        }
+        else
+        {
+            return Content("Address not changed");
+        }
+    }
+
+    [HttpPut("change-password-user")]
+    public ContentResult ChangePassword(string newPassword)
+    {
+        bool result = Service.UserService.GetUserService().ChangePassword(newPassword);
+        if (result)
+        {
+            return Content("Password changed");
+        }
+        else
+        {
+            return Content("Password not changed");
+        }
+    }
+
+    [HttpPost("change-name-user")]
+    public ContentResult ChangeName(string name)
+    {
+        bool result = Service.UserService.GetUserService().ChangeName(name);
+        if (result)
+        {
+            return Content("Name changed");
+        }
+        else
+        {
+            return Content("Name not changed");
+        }
+       
+    }
+}
